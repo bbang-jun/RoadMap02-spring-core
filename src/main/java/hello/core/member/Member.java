0a0f6b17e7a59a526b34a45b71e1682803ba698b
bbangjun <lucid9dawn@gmail.com>
2023-02-18 17:08:17 +0900

    feat(MemberService): Add MemberService interface_회원 도메인 개발

--- src/main/java/hello/core/member/Member.java
+++ src/main/java/hello/core/member/Member.java
@@ -1,6 +1,17 @@
 package hello.core.member;
 
 public class Member {
+
+    private Long id;
+    private String name;
+    private Grade grade;
+
+    public Member(Long id, String name, Grade grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+
     public Long getId() {
         return id;
     }
@@ -22,16 +33,6 @@ public class Member {
     }
 
     public void setGrade(Grade grade) {
-        this.grade = grade;
-    }
-
-    private Long id;
-    private String name;
-    private Grade grade;
-
-    public Member(Long id, String name, Grade grade) {
-        this.id = id;
-        this.name = name;
         this.grade = grade;
     }
 }
