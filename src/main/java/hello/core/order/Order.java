6f9fa11e3d2469976496747823c1d06afa349df0
bbangjun <lucid9dawn@gmail.com>
2023-02-18 18:34:29 +0900

    feat(Order): Add 변수, getter/setter, constructor, toString()_주문과 할인 도메인 개발

---
+++ src/main/java/hello/core/order/Order.java
@@ -0,0 +1,63 @@
+package hello.core.order;
+
+public class Order {
+
+    private Long memberId;
+    private String itemName;
+    private int itemPrice;
+    private int discountPrice;
+
+    public Order(Long memberId, String itemName, int itemPrice, int discountPrice) {
+        this.memberId = memberId;
+        this.itemName = itemName;
+        this.itemPrice = itemPrice; // 원가
+        this.discountPrice = discountPrice; // 할인해주는 가격(할인 후 가격x)
+    }
+
+    public int calculatePrice(){
+        return itemPrice - discountPrice;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(int itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    public int getDiscountPrice() {
+        return discountPrice;
+    }
+
+    public void setDiscountPrice(int discountPrice) {
+        this.discountPrice = discountPrice;
+    }
+
+    // 정보를 보기 쉽게 하기 위해 생성(객체 출력 시 toString()의 결과 출력)
+    @Override
+    public String toString() {
+        return "Order{" +
+                "memberId=" + memberId +
+                ", itemName='" + itemName + '\'' +
+                ", itemPrice=" + itemPrice +
+                ", discountPrice=" + discountPrice +
+                '}';
+    }
+}
